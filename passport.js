bae042fea1b1a36c043703519f72844104a88668
Antone-2 <antonebruele103@gmail.com>
2025-10-07 13:54:23 +0300

    Update callbackURL to remove default localhost

--- passport.js
+++ passport.js
@@ -9,7 +9,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.BACKEND_URL || 'http://localhost:5000'}/api/v1/auth/google/callback`,
+        callbackURL: `${process.env.BACKEND_URL }/api/v1/auth/google/callback`,
     },
         async (accessToken, refreshToken, profile, done) => {
             try {
